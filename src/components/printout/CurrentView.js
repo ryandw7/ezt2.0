027319b990ce203e6340bf5581d9c9b83d70e104
ryan <ryandevlinwilson@gmail.com>
2025-03-20 20:14:33 -0400

    add tv and mobile service to core print out

--- src/components/printout/CurrentView.js
+++ src/components/printout/CurrentView.js
@@ -3,11 +3,19 @@ import { Box, Typography, Paper } from "@mui/material"
 const CurrentView = ({ currentServices }) => {
     const { internet, internetCost, tv, tvCost, mobile, mobileCost, notes } = currentServices;
     return (
-        <Paper elevation={3} sx={{ p: "0px", width: "100%", borderTopRightRadius: "20px", borderTopLeftRadius: "20px", backgroundColor: "#F5F5F5" }} className="current-view">
-            <Typography variant='h2' sx={{ height:"60px",width: "100%", borderTopRightRadius: "20px", borderTopLeftRadius: "20px", m: "0 auto", p: "10px" }}>Current Services</Typography>
-            <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-around", justifyContent:"space-around",width:"100%"}}>
-                <Box sx={{textAlign:"center", height:"max-content"}}>
-                    <Typography variant="h3">Internet: {internetCost}</Typography>
+        <Paper elevation={3} sx={{ p: "0px", width: "100%", borderTopRightRadius: "20px", borderTopLeftRadius: "20px", backgroundColor: "#F5F5F5", border: "3px solid black" }} className="current-view">
+            <Typography variant='h2' sx={{ height:"60px",width: "100%", borderTopRightRadius: "20px", borderTopLeftRadius: "20px", m: "0 auto", p: "10px"}}>Current Services</Typography>
+            <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-around", justifyContent:"space-around",width:"100%", height:"calc(100% - 60px)"}}>
+                <Box sx={{textAlign:"center"}}>
+                    <Typography variant="h3">Internet: ${internetCost.toFixed(2)}</Typography>
+                    <Typography>{internet}</Typography>
+                </Box>
+                <Box sx={{textAlign:"center"}}>
+                    <Typography variant="h3">TV & Extras: ${tvCost.toFixed(2)}</Typography>
+                    <Typography>{internet}</Typography>
+                </Box>
+                <Box sx={{textAlign:"center"}}>
+                    <Typography variant="h3">Mobile Service: ${mobileCost.toFixed(2)}</Typography>
                     <Typography>{internet}</Typography>
                 </Box>
             </Box>
