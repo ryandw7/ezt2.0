08456cc69177b35c981f42e5f17112d92bbc49e3
ryan <ryandevlinwilson@gmail.com>
2025-04-05 22:03:44 -0400

    remove p from new view

--- src/components/printout/NewView.js
+++ src/components/printout/NewView.js
@@ -6,7 +6,7 @@ const NewView = ({ newCore, mobileCost, newTotal }) => {
     const { taxes, xmcTotal, unlimitedLines, unlimitedPlus, watches, tablets, deviceMonthly, lineQuantity, mobileTotal, lineDiscounts } = mobileCost;
 
     return (
-        <Paper elevation={3} sx={{ p: "0px", borderTopRightRadius: "20px", borderTopLeftRadius: "20px", backgroundColor: "#F5F5F5" }} className="new-view">
+        <Paper elevation={3} sx={{ borderTopRightRadius: "20px", borderTopLeftRadius: "20px", backgroundColor: "#F5F5F5" }} className="new-view">
             <Typography variant='h2' sx={{ height: "60px", width: "100%", borderTopRightRadius: "20px", borderTopLeftRadius: "20px", m: "0 auto", p: "10px" }}>New Services</Typography>
             <Box sx={{ display: "flex", flexDirection: "column", alignItems: "space-between", justifyContent: "flex-start", position: "relative", width: "100%", height: "calc(100% - 60px)" }}>
 
