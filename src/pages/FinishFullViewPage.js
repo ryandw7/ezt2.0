8d1f9865bb31364eb3ec377164e4984af49ef0cb
Ryan Wilson <ryandevlinwilson@gmail.com>
2025-08-17 16:18:09 +0000

    remove typo from finish full view

--- src/pages/FinishFullViewPage.js
+++ src/pages/FinishFullViewPage.js
@@ -1,6 +1,5 @@
-cimport React from 'react';
+import React from 'react';
 import FinishFeature from '../features/FinshFeature';
-import { ContextProvider } from '../context/context';
 
 const FinishFullView = () => {
   return <FinishFeature isFullView={true} paperHeight="90vh" fontSize="11pt" />;
