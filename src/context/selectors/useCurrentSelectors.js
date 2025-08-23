9b06bee89d1ab454ab5ab28819f0e7ae725a5ede
Ryan Wilson <ryandevlinwilson@gmail.com>
2025-08-23 14:20:35 +0000

    remove unused values from useCurrentSelectors

--- src/context/selectors/useCurrentSelectors.js
+++ src/context/selectors/useCurrentSelectors.js
@@ -28,18 +28,6 @@ export const getCurrentServicesItemValueById = createSelector(
     };
   }
 );
-export const getCurrentInternet = (state) =>
-  state.currentServices?.internet || '';
-export const getCurrentInternetCost = (state) =>
-  state.currentServices?.internetCost || 0;
-export const getCurrentTv = (state) => state.currentServices?.tv || '';
-export const getCurrentTvCost = (state) => state.currentServices?.tvCost || 0;
-export const getCurrentMobile = (state) => state.currentServices?.mobile || '';
-export const getCurrentMobileCost = (state) =>
-  state.currentServices?.mobileCost || 0;
-export const getCurrentNotes = (state) => state.currentServices?.notes || '';
-export const getCurrent = (state) => state.currentServices;
-export const getCurrentServices = (state) => state.currentServices?.items || [];
 
 export const getCurrentServicesTotalCost = createSelector(
   [getCurrentServicesItems],
@@ -56,30 +44,16 @@ const useCurrentSelectors = () => {
     console.error('State is undefined in useAppSelectors');
     return {};
   }
-
-  const currentInternet = getCurrentInternet(state);
-  const currentInternetCost = getCurrentInternetCost(state) || 0;
-  const currentTv = getCurrentTv(state) || '';
-  const currentTvCost = getCurrentTvCost(state);
-  const currentMobile = getCurrentMobile(state) || '';
-  const currentMobileCost = getCurrentMobileCost(state);
-  const currentNotes = getCurrentNotes(state) || '';
-  const current = getCurrent(state) || '';
-  const currentServicesTotalCost = getCurrentServicesTotalCost(state) || 0;
-  const currentServices = getCurrentServices(state);
+  const currentServicesItems = getCurrentServicesItems(state);
+  const currentServicesItemById = getCurrentServicesItemById(state);
   const currentServicesItemsList = getCurrentServicesItemsList(state);
+  const currentServicesTotalCost = getCurrentServicesTotalCost(state) || 0;
+  
   return {
+    currentServicesItems,
+    currentServicesItemById,
     currentServicesItemsList,
-    currentServices,
-    current,
     currentServicesTotalCost,
-    currentInternet,
-    currentInternetCost,
-    currentTv,
-    currentTvCost,
-    currentMobile,
-    currentMobileCost,
-    currentNotes,
   };
 };
 
